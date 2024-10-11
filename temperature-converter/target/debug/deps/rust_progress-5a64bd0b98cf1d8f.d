8bd535ff4550c08d9d9aad53b2e6762de43eb8cf
AzrielTheHellrazor <goktug200402@gmail.com>
2024-10-11 14:04:21 +0300

    fix

---
+++ temperature-converter/target/debug/deps/rust_progress-5a64bd0b98cf1d8f.d
@@ -0,0 +1,5 @@
+C:\Users\Azriel\Desktop\rust-progress\target\debug\deps\librust_progress-5a64bd0b98cf1d8f.rmeta: src/main.rs
+
+C:\Users\Azriel\Desktop\rust-progress\target\debug\deps\rust_progress-5a64bd0b98cf1d8f.d: src/main.rs
+
+src/main.rs:
---
+++ temperature-converter/target/debug/deps/rust_progress-e62af592bbb956b2.d
@@ -0,0 +1,5 @@
+C:\Users\Azriel\Desktop\rust-progress\target\debug\deps\librust_progress-e62af592bbb956b2.rmeta: src/main.rs
+
+C:\Users\Azriel\Desktop\rust-progress\target\debug\deps\rust_progress-e62af592bbb956b2.d: src/main.rs
+
+src/main.rs:
